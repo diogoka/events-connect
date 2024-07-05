5fca611be942396914b45a23b0d72f6d6f010ec1
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2024-07-05 10:23:12 -0700

    cors issue

--- backend/app.ts
+++ backend/app.ts
@@ -21,17 +21,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://cornerstone-connect.onrender.com/'
-  );
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Headers', 'application/json');
   res.setHeader('Access-Control-Allow-Headers', 'Authorization');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'https://cornerstone-connect.onrender.com/'
-  );
+  res.setHeader('Access-Control-Allow-Methods', '*');
   next();
 });
 
