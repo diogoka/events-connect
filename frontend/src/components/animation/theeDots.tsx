37c29b1ab385025b3ab6f244dd1ff13e38ff260f
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2024-07-09 09:07:20 -0700

    Increasing time & adding animation

--- frontend/src/components/animation/theeDots.tsx
+++ frontend/src/components/animation/theeDots.tsx
@@ -3,50 +3,57 @@ import React from 'react';
 import { keyframes } from '@emotion/react';
 import { Box, styled } from '@mui/system';
 
-const dotFlashing = keyframes`
+type Props = {
+  color?: string;
+};
+
+const ThreeDots = ({ color }: Props) => {
+  const setColor = color ? color : 'black';
+  const rgb = color ? 'rgba(78, 78, 78, 0.5)' : 'rgba(78, 78, 78, 0.2)';
+
+  const dotFlashing = keyframes`
   0% {
-    background-color: black;
+    background-color: ${setColor};
   }
   50%, 100% {
-    background-color: rgba(78, 78, 78, 0.2);
+    background-color: ${rgb}
   }
 `;
 
-const Dot = styled('div')({
-  position: 'relative',
-  width: 10,
-  height: 10,
-  borderRadius: 5,
-  backgroundColor: 'black',
-  animation: `${dotFlashing} 1s infinite linear alternate`,
-  animationDelay: '0.5s',
-  '&::before, &::after': {
-    content: '""',
-    display: 'inline-block',
-    position: 'absolute',
-    top: 0,
-  },
-  '&::before': {
-    left: -15,
+  const Dot = styled('div')({
+    position: 'relative',
     width: 10,
     height: 10,
     borderRadius: 5,
-    backgroundColor: 'black',
-    animation: `${dotFlashing} 1s infinite alternate`,
-    animationDelay: '0s',
-  },
-  '&::after': {
-    left: 15,
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    backgroundColor: 'black',
-    animation: `${dotFlashing} 1s infinite alternate`,
-    animationDelay: '1s',
-  },
-});
+    backgroundColor: `${setColor}`,
+    animation: `${dotFlashing} 1s infinite linear alternate`,
+    animationDelay: '0.5s',
+    '&::before, &::after': {
+      content: '""',
+      display: 'inline-block',
+      position: 'absolute',
+      top: 0,
+    },
+    '&::before': {
+      left: -15,
+      width: 10,
+      height: 10,
+      borderRadius: 5,
+      backgroundColor: `${setColor}`,
+      animation: `${dotFlashing} 1s infinite alternate`,
+      animationDelay: '0s',
+    },
+    '&::after': {
+      left: 15,
+      width: 10,
+      height: 10,
+      borderRadius: 5,
+      backgroundColor: `${setColor}`,
+      animation: `${dotFlashing} 1s infinite alternate`,
+      animationDelay: '1s',
+    },
+  });
 
-const ThreeDots = () => {
   return (
     <Box
       sx={{
